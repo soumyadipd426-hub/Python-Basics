9b523470d648318ec1195e352d87385f1ed852f8
Soumyadip <Soumyadipd426@gmail.com>
2026-08-05 13:55:04 +0530

    add python basic practice

---
+++ basics.py
@@ -0,0 +1,25 @@
+#print("Hii")
+
+#Strings
+Name="Soumyadip"
+Address="Rayasandra"
+
+#integers
+roll=32
+marks=89
+
+print(f"{Name} got {marks} in total")
+print(f"He lives in {Address}")
+
+#float 
+gpa = 8.5
+
+print(f"his gpa is {gpa}")
+
+#boolean
+
+is_student=True
+if is_student:
+    print("He is a student")
+else:
+    print("He is not a student")
