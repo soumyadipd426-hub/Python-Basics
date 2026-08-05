0be301f529f7ceb10fd3edfdf8866cd349600df6
Soumyadip <Soumyadipd426@gmail.com>
2026-08-05 14:31:49 +0530

    add program to calculate Area of triangle

---
+++ area_of_triangle.py
@@ -0,0 +1,8 @@
+#Calculate area of triangle
+print("Calculate area of triangle!")
+length=float(input("Enter the length:"))
+width=float(input("Enter the width:"))
+
+Area=length*width
+
+print(f"The area of triangle is {Area} cm²")
