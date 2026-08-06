8b17a11dddc0884113f80c927a32e6caccdf4c8d
Soumyadip <Soumyadipd426@gmail.com>
2026-08-06 14:31:39 +0530

    add use of arithmetic operator to calculate area of circle

---
+++ area_of_circle.py
@@ -0,0 +1,9 @@
+import math
+print("Calculate area of circle!")
+#a=pi r square
+
+r=float(input("Enter the radius of circle: "))
+
+a=math.pi*pow(r,2)
+
+print(f"The area of circle is {round(a,2)} cm^2")
