e6d156d6b753f8c115da532c7aac7f94770bd510
Soumyadip <Soumyadipd426@gmail.com>
2026-08-07 14:31:18 +0530

    add fix divide by zero error and bug fixes

--- calculator.py
+++ calculator.py
@@ -3,18 +3,30 @@ operator=input("What operator do you wanna run? (+,-,*,/,%) :")
 num1=float(input("Enter first number:"))
 num2=float(input("Enter second number:"))
 
+
 if operator=="+":
     result=num1+num2
 elif operator=="-":
     result=num1-num2
 elif operator=="*":
-    result=num1-num2
-elif operator=="*":
-    result=num1-num2
+    result=num1*num2
 elif operator=="/":
-    result=num1/num2
+    
+    if num2!=0:
+        
+        if num1!=0:
+            result=num1/num2
+        else:
+            print(f"The value of {num1} {operator} {num2} is 0")
+            result=""
+    else:
+        print(f"The value of {num1} {operator} {num2} is infinity")
+        result=""
+    
 elif operator=="%":
     result=num1%num2
 else:
     print("Invalid")
-print(f"The value of {num1} {operator} {num2} is {round(result,2)}")
+    result=""
+if result:
+    print(f"The value of {num1} {operator} {num2} is {round(result,2)}")
