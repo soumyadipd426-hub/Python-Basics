6f929966a6fab385b16120e787a5f3dfbbaa235f
Soumyadip <Soumyadipd426@gmail.com>
2026-08-29 19:17:37 +0530

    add bug fixes and organisation

--- compound_interest_calculator.py
+++ compound_interest_calculator.py
@@ -1,20 +1,31 @@
 #a=p(1+r)^t
 print("[Compound Interest Calculator]")
+principle=0
+rate=0
+time=0
 
-principle=float(input("Enter the principle value:"))
-rate=float(input("Enter the rate of interest /year:"))
-#rate should be 0 to 100
-while rate<0 or rate>100:
-    print("Rate is invalid!")
+while principle<=0: 
+    principle=float(input("Enter the principle value:"))
+    if principle<=0:
+        print("Invalid")
+
+while rate<=0 or rate>100: 
     rate=float(input("Enter the rate of interest /year:"))
+    if rate<=0:
+        print("Invalid")
+    elif rate>100:
+        print("Invalid")
 
-time=int(input("Enter the Time period in years:"))
+while time<=0: 
+    time=int(input("Enter the Time period in years:"))
+    if time<=0:
+        print("Invalid")
 
 final=principle*((1+(rate/100))**time)
 
 compound=final-principle
 
-print(f"The final amount is {final: .2f}\n ")
+print(f"The final amount after {time} Years is {final: .2f}\n ")
 print(f"The Compound interest is {compound: .2f}")
 
 
