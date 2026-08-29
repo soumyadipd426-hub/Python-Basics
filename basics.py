4b5efd9a0b80138f2aabafa0fa69b04911323839
Soumyadip <Soumyadipd426@gmail.com>
2026-08-29 14:28:18 +0530

    add while loop basics

--- basics.py
+++ basics.py
@@ -41,13 +41,30 @@
 
 #string methods
 
-username=input("Enter Your Username:")
+#username=input("Enter Your Username:")
 
-if len(username)>12:
-    print("Username can't be more than 12 Characters!")
-elif not username.find(" ")== -1:
-    print("Username can't contain spaces!")
-elif not username.isalpha():
-    print("Username can't contain numbers!")
-else:
-    print(f"Welcome {username}")
+#if len(username)>12:
+    #print("Username can't be more than 12 Characters!")
+#elif not username.find(" ")== -1:
+#    print("Username can't contain spaces!")
+#elif not username.isalpha():
+#    print("Username can't contain numbers!")
+#else:
+#    print(f"Welcome {username}")
+
+#while loop
+
+age=int(input("Enter your age:"))
+
+while age<0:
+    print("Age cant be negative!")
+    age=int(input("Enter your age:"))
+print(f"You are {age} year old")
+
+num=int(input("Enter a number between 1 - 10 : "))
+
+while num<1 or num>10:
+    print(f"{num} is not valid")
+    num=int(input("Enter a number between 1 - 10 : "))
+print(f"Your number is {num}")
+    
