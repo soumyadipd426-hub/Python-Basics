18df3208ce152c8f48e242401de498b145b976b6
Soumyadip <Soumyadipd426@gmail.com>
2026-08-29 14:17:01 +0530

    add string method basics

--- basics.py
+++ basics.py
@@ -1,40 +1,53 @@
 #print("Hii")
 
 #Strings
-Name="Soumyadip"
-Address="Rayasandra"
+#Name="Soumyadip"
+#Address="Rayasandra"
 
 #integers
-roll=32
-marks=89
+#roll=32
+#marks=89
 
-print(f"{Name} got {marks} in total")
-print(f"He lives in {Address}")
+#print(f"{Name} got {marks} in total")
+#print(f"He lives in {Address}")
 
 #float 
-gpa = 8.5
+#gpa = 8.5
 
-print(f"his gpa is {gpa}")
+#print(f"his gpa is {gpa}")
 
 #boolean
 
-is_student=True
-if is_student:
-    print("He is a student")
-else:
-    print("He is not a student")
+#is_student=True
+#if is_student:
+ #   print("He is a student")
+#else:
+ #   print("He is not a student")
 
 #typecasting
-roll=float(roll)
+#roll=float(roll)
 
-print(roll)
-print(type(roll))
+#print(roll)
+#print(type(roll))
 
-Name=bool(Name)
-print(Name)
+#Name=bool(Name)
+#print(Name)
 
 #input
 
-Name=input("Enter your name:")
-age=int(input("Enter your age:"))
-print(f"Hello {Name}! \nYou are {age} year old")
+#Name=input("Enter your name:")
+#age=int(input("Enter your age:"))
+#print(f"Hello {Name}! \nYou are {age} year old")
+
+#string methods
+
+username=input("Enter Your Username:")
+
+if len(username)>12:
+    print("Username can't be more than 12 Characters!")
+elif not username.find(" ")== -1:
+    print("Username can't contain spaces!")
+elif not username.isalpha():
+    print("Username can't contain numbers!")
+else:
+    print(f"Welcome {username}")
